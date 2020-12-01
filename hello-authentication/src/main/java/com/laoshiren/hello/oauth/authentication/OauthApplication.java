8544fce031eeaef74b6a7147834086cff3d136db
laoshiren <15207034473@163.com>
2020-12-01 17:40:41 +0800

    2020-12-01 17:40:33 init repo laoshiren

---
+++ hello-authentication/src/main/java/com/laoshiren/hello/oauth/authentication/OauthApplication.java
@@ -0,0 +1,23 @@
+package com.laoshiren.hello.oauth.authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ProjectName:     hello-oauth
+ * Package:         com.laoshiren.hello.oauth.authentication
+ * ClassName:       OauthApplication
+ * Author:          laoshiren
+ * Git:             xiangdehua@pharmakeyring.com
+ * Description:
+ * Date:            2020/12/1 17:36
+ * Version:         1.0.0
+ */
+@SpringBootApplication
+public class OauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OauthApplication.class,args);
+    }
+
+}
